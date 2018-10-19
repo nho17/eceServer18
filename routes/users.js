19049aa3b3b3279f9791504e285d13c342c54e17
Roman Lysecky <roman@lysecky.com>
2018-10-19 16:08:32 -0700

    Added authentication.

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,34 @@ var router = express.Router();
 var fs = require('fs');
 var User = require("../models/users");
 var bcrypt = require("bcrypt-nodejs");
+var jwt = require("jwt-simple");
+
+/* Authenticate user */
+var secret = "supersecretserverpassword";
+router.post('/signin', function(req, res, next) {
+   User.findOne({email: req.body.email}, function(err, user) {
+      if (err) {
+         res.status(401).json({success : false, error : "Error communicating with database."});
+      }
+      else if(!user) {
+         res.status(401).json({success : false, error : "The email or password provided was invalid."});         
+      }
+      else {
+         bcrypt.compare(req.body.password, user.passwordHash, function(err, valid) {
+            if (err) {
+               res.status(401).json({success : false, error : "Error authenticating. Please contact support."});
+            }
+            else if(valid) {
+               var token = jwt.encode({email: req.body.email}, secret);
+               res.status(201).json({success : true, token : token});         
+            }
+            else {
+               res.status(401).json({success : false, error : "The email or password provided was invalid."});         
+            }
+         });
+      }
+   });
+});
 
 /* Register a new user */
 router.post('/register', function(req, res, next) {
